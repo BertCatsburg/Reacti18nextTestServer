ca6c2ebb52a05637abd977441e43cbca3d5b1ad4
bert <bert@catsburg.com>
2021-05-10 09:45:29 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3014
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// app.get('/getlanguage', (req, res) => {
+//
+// })
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
